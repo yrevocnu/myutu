9e5c422c1cfdda98466884bba28c17a7f8211a6b
Dav Clark <ClarkDa@kennedykrieger.org>
2017-03-11 10:39:11 -0500

    Did something

--- myutu-clj/README.md
+++ myutu-clj/README.md
@@ -2,6 +2,16 @@
 
 FIXME: description
 
+I'm leaving boilerplate above and below for now. But if you want to try this
+out, install leiningen (follow instructions on their website). Then from this
+directory:
+
+  $ lein install
+  $ lein run
+
+You'll exercise the `-main` function in `src/myutu_clj/core.clj`, which both
+parses and generates some YAML. No reading from files yet.
+
 ## Installation
 
 Download from http://example.com/FIXME.
--- myutu-clj/src/myutu_clj/core.clj
+++ myutu-clj/src/myutu_clj/core.clj
@@ -1,7 +1,18 @@
 (ns myutu-clj.core
   (:gen-class))
 
+; Note sure if require conventionally comes before or after ns
+(require '[clj-yaml.core :as yaml])
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println
+    (yaml/generate-string
+      [{:name "John Smith", :age 33}
+       {:name "Mary Smith", :age 27}])
+    (yaml/parse-string "
+    - {name: John Smith, age: 33}
+    - name: Mary Smith
+      age: 27
+    ") ))
